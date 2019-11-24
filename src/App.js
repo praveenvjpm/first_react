16823c75ce25a6736e2ddfed84e8ab62019bf34f
praveenvjpm <praveenvjpm@gmail.com>
2019-11-24 22:07:46 +0530

    Updated vesion

--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
-import React,{Component} from 'react'; 
-import './App.css';
-import { CardList} from './components/card-list/card-list.component';
-import {SearchBox} from './components/search-box/search-box.component'
+import React, { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 
-class App extends Component{
+class App extends Component {
+  constructor() {
+    super();
 
+    this.state = {
+      monsters: [],
+      searchField: ""
+    };
+    //this.handleChange = this.handleChange.bind(this);
+  }
 
-constructor(){
-  super();
- 
-  this.state = {
-   monsters: [],
-   searchField: ''
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
+
+  handleChange = e => {
+    this.setState({
+      searchField: e.target.value
+    });
   };
-}
 
-componentDidMount(){
-  fetch('https://jsonplaceholder.typicode.com/users').then(
-    response => response.json()
-    ).then(
-     users => this.setState({monsters: users})
+  render() {
+    const { monsters, searchField: x } = this.state;
+    const filterMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(x.toLowerCase())
     );
-}
 
-  render(){
-
-    const { monsters,searchField:x } = this.state;
-    const filterMonsters = monsters.filter(
-      monster => monster.name.toLowerCase().includes(x.toLowerCase())
-      );
-      
     return (
-      
-        <div className='App'>
-         
-          <SearchBox placeholder='search monsters' handleChange={e => this.setState({
-            searchField: e.target.value
-           } )} />
-          <CardList monsters={filterMonsters}/>
-        </div>
-      
+      <div className="App">
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filterMonsters} />
+      </div>
     );
   }
 }
